58d6e11e83e2086a73b94f838d6a661345c01773
Ari Greenberg <agreenberg@lyft.com>
2020-03-15 22:09:01 -0700

    Solve 18

---
+++ leetcode/18.py
@@ -0,0 +1,77 @@
+# 18. 4Sum
+# Given an array nums of n integers and an integer target, are there elements a, b, c, and d in nums
+# such that a + b + c + d = target? Find all unique quadruplets in the array which gives the sum of target.
+# Note: The solution set must not contain duplicate quadruplets.
+# There have been many similar problems: 2-Sum, 3-Sum, and now 4-Sum.
+# I will solve the general K-sum solution
+from typing import List
+
+
+def k_sum(nums: List[int], target: int, k: int) -> List[List[int]]:
+    def k_sum_sorted(nums: List[int], target: int, k: int) -> List[List[int]]:
+        """
+        Assumes that nums has been sorted and that k >= 2.
+        """
+        results: List[List[int]] = []
+        if k == 2:
+            left = 0
+            right = len(nums) - 1
+            while left < right:
+                if nums[left] + nums[right] < target:
+                    left += 1
+                elif nums[left] + nums[right] > target:
+                    right -= 1
+                else:
+                    results.append([nums[left], nums[right]])
+                    left += 1
+                    right -= 1
+        else:
+            for first_index in range(len(nums)):
+                new_target = target - nums[first_index]
+                sub_results = k_sum_sorted(nums[first_index + 1 :], new_target, k - 1)
+                combined_results = [
+                    [nums[first_index]] + sub_result for sub_result in sub_results
+                ]
+                results += combined_results
+
+        return results
+
+    if k <= 0 or k > len(nums):
+        results: List[List[int]] = []
+    elif k == 1:
+        results = [[target]] if target in nums else []
+    else:
+        results = k_sum_sorted(sorted(nums), target, k)
+
+    # Uniqify results
+    return [list(result) for result in {tuple(result) for result in results}]
+
+
+class Solution:
+    def fourSum(self, nums: List[int], target: int) -> List[List[int]]:
+        return k_sum(nums, target, 4)
+
+
+try:
+    import pytest
+
+    @pytest.mark.parametrize(
+        ("nums", "target", "k", "expected"),
+        [
+            ([1, 2, 2, 3], 4, 2, [[1, 3], [2, 2]]),
+            (
+                [1, 0, -1, 0, -2, 2],
+                0,
+                4,
+                [[-1, 0, 0, 1], [-2, -1, 1, 2], [-2, 0, 0, 2]],
+            ),
+        ],
+    )
+    def test_k_sum(
+        nums: List[int], target: int, k: int, expected: List[List[int]]
+    ) -> None:
+        assert sorted(k_sum(nums, target, k)) == sorted(expected)
+
+
+except ModuleNotFoundError:
+    pass
