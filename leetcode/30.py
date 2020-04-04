689e1bda86f9bf9075692321522231f4d3df874e
Ari Greenberg <agreenberg@lyft.com>
2020-04-03 21:20:33 -0700

    Solve 30 by stripes (76 ms, faster than 85.95%)

--- leetcode/30.py
+++ leetcode/30.py
@@ -9,6 +9,7 @@
 #    i, generate construct a counter of the num_words starting at i. Compare the
 #    counters to see if they match. If they do, i is a solution.
 # 3. Return a List containing all valid i's found in step 2.
+import itertools
 from collections import Counter
 from typing import Counter as CounterType
 from typing import List
@@ -17,7 +18,10 @@ from utils import timing
 
 
 class Solution:
-    def findSubstring(self, s: str, words: List[str]) -> List[int]:
+    def findSubstringNaive(self, s: str, words: List[str]) -> List[int]:
+        # For each position in S, check to see if the starting position is value
+        # Worst case run time: O(s * n) where s is length of string and n is num
+        # words
         if not words:
             return []
         num_words = len(words)
@@ -50,6 +54,46 @@ class Solution:
             i += word_length
         return True
 
+    def findSubstringOnePass(self, s: str, words: List[str]) -> List[int]:
+        # O(s / word_length) *
+
+        def get_stripe_results(i: int) -> List[int]:
+            results = []
+            counts: CounterType[str] = Counter()
+            start = i
+            end = i
+            while end + word_length <= len(s):
+                word = s[end : end + word_length]
+                end += word_length
+                if word not in template:
+                    start = end
+                    counts.clear()
+                else:
+                    counts[word] += 1
+                    if counts[word] > template[word]:
+                        # pop off from the beginning
+                        while True:
+                            current_word = s[start : start + word_length]
+                            counts[current_word] -= 1
+                            start += word_length
+                            if current_word == word:
+                                break
+                if end - start == total_length:
+                    results.append(start)
+            return results
+
+        if not words:
+            return []
+        num_words = len(words)
+        word_length = len(words[0])
+        total_length = num_words * word_length
+        template: CounterType[str] = Counter(words)
+        results_lists = [get_stripe_results(i) for i in range(word_length)]
+        return list(itertools.chain(*results_lists))
+
+    def findSubstring(self, s: str, words: List[str]) -> List[int]:
+        return self.findSubstringOnePass(s, words)
+
 
 try:
     import pytest
@@ -62,11 +106,14 @@ try:
             ("birdword", ["bird", "word"], [0]),
             ("wordgoodgoodgoodbestword", ["word", "good", "best", "word"], []),
             ("wordgoodgoodgoodbestword", ["good", "good"], [4, 8]),
+            ("barfoothefoobarman", ["foo", "bar"], [0, 9]),
         ],
     )
     def testFindSubstring(s: str, words: List[str], expected: List[str]) -> None:
         with timing(f"naive"):
-            result = Solution().findSubstring(s, words)
+            result = Solution().findSubstringNaive(s, words)
+        with timing(f"onepass"):
+            result = Solution().findSubstringOnePass(s, words)
         assert sorted(result) == sorted(expected)
 
 
